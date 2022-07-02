82afd21989db287ca5c200bbb64b156fd441c95c
emmamarks27 <100089874+emmamarks27@users.noreply.github.com>
2022-07-03 00:02:09 +0100

    Additional styling

--- src/java.js
+++ src/java.js
@@ -1,13 +1,18 @@
 function handleSubmit(event) {
   event.preventDefault();
   checkLength();
+  displayComment();
 }
 
 function commentLength() {
   commentBoxLength = commentBox.value.length;
   let displayCharacterLenght = document.querySelector("#comment-box-length");
+  if (commentBoxLength > 140) {
+    commentBox.classList.add("exceed-character-limit");
+  } else {
+    commentBox.classList.remove("exceed-character-limit");
+  }
   displayCharacterLenght.innerHTML = commentBoxLength;
-  //checkLength(commentBoxLength);
   return commentBoxLength;
 }
 
@@ -17,6 +22,20 @@ function checkLength() {
   }
 }
 
+function displayComment() {
+  let name = document.querySelector("#Name").value;
+  let comment = document.querySelector(".comment-display");
+  if (name.length > 0) {
+    feedBack.push(`${name} wrote: ${commentBox.value}`);
+  } else {
+    feedBack.push(`User wrote: ${commentBox.value}`);
+  }
+
+  comment.innerHTML = feedBack;
+  comment.classList.add("section");
+  console.log(comment);
+}
+let feedBack = [];
 let submit = document.querySelector("#form");
 let commentBox = document.querySelector("#comment-box");
 let commentBoxLength = 0;
